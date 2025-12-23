a5e1c4bfcc9c9372e9106b00e52d28a86958bf2b
unknown <martin@joyouscoding.com>
2025-12-23 12:07:37 +0100

    new data!

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,21 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-EffinQuote.create([
+require 'json'
+
+posts_file = File.read(Rails.root.join('posts.json'))
+posts = JSON.parse(posts_file)
+
+quotes_data = posts.map do |post|
   {
-    contents: "good luck with your bullshit",
-    url: "https://pbs.twimg.com/media/DQ2tB-xWkAAKKJh.jpg"
-  },
-  {
-    contents: "got it you're dim",
-    url: "https://pbs.twimg.com/media/DQ117aPW4AEmnWF.jpg"
-  },
-  {
-    contents: "where is your fucking brain",
-    url: "https://pbs.twimg.com/media/DQxjO2fWsAEFIMd.jpg"
-  },
-  {
-    contents: "I am not your fucking mom",
-    url: "https://pbs.twimg.com/media/DQwsRAzXcAAIV-n.jpg"
+    contents: post['alt_text'],
+    url: post['image_url'],
+    twitter_url: post['post_url']
   }
-])
+end
+
+EffinQuote.create(quotes_data)
